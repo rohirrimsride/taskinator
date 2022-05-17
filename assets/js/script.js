bd90d47251895fc2c0d749cb6c3fce6aab8ed7d5
Strider <dave.barnes23@gmail.com>
2022-05-17 14:20:10 -0500

    added ability to submit a new task and have it persist

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+       // prevents automatic browser refresh and subsequent loss of submitted data
+        event.preventDefault();
+
         // creates a new <li> element and a resulting new task item
         var listItemEl = document.createElement("li");
         // assigns css class styling to new <li> element
@@ -10,6 +13,6 @@ var createTaskHandler = function() {
         listItemEl.textContent = "This is a new task.";
         // appends new child <li> element to bottom of the parent <ul> element
         tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
